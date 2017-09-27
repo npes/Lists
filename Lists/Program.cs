5040639fa24a9fe362c3b594bffb584a26447839
npes <npesdk@gmail.com>
2017-09-27 15:47:14 +0200

    Program done

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Lists
 {
@@ -10,6 +10,31 @@ namespace Lists
     {
         static void Main(string[] args)
         {
+            List<string> myCars = new List<string>();
+            myCars.Add("Toyota");
+            myCars.Add("BMW");
+            myCars.Add("Skoda");
+            myCars.Add("Audi");
+            Console.WriteLine("I have these cars:");
+            foreach (string car in myCars)
+            {
+                
+                Console.WriteLine(car);
+            }
+            Console.WriteLine("What car do you have?");
+            var usersCar = Console.ReadLine();
+
+            myCars.Add(usersCar);
+
+            Console.WriteLine("Your car is added to my list and now contains:");
+
+            foreach (string car in myCars)
+            {
+
+                Console.WriteLine(car);
+            }
+
+            Console.ReadLine();
         }
     }
 }
